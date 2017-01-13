3d062f49699ca12aba299009f5e63e487c20c01d
brezniczky <brezniczky@gmail.com>
2017-01-13 17:30:29 +0100

    Added initial StackExchange scraping code and results.
    Relates to #17.

---
+++ so-client.R
@@ -0,0 +1,255 @@
+# TODO: save all responses in perhaps a database or JSON
+#       for later semantic analysis
+# TODO: sparse scraping
+
+# stackr::stack_questions() does not seem to support 
+# by label & body text filtering, so left with querying
+# the StackExchange API in a more generic way
+
+# StackOverflow has at least a one second granualarity
+# for specifying time ranges, therefore 
+library(httr)
+
+retry = function(expr, times) {
+  
+  retried = 0
+  
+  expr = substitute(expr)
+  
+  while (retried < times) {
+    tryCatch({
+      eval(expr, envir = parent.frame())
+      break
+    },
+    error = function(e) {
+      retried <<- retried + 1
+      if (retried < times) {
+        print(e)
+        print("retrying ...")
+      } else {
+        stop(e)
+      }
+    })
+  }
+}
+
+# If this one returns has_more=TRUE in the attributes of the result,
+# the results are to be considered unreliable.
+get.SO.question.count = function(fromdate, todate, body, tagged, 
+                                 app.key, wait.it = TRUE, num.retries = 3) {
+  # url = 
+  #   'http://api.stackexchange.com/2.2/search/advanced?' +
+  #   'fromdate=1483228800&todate=1483660800&order=desc&' +
+  #   'sort=activity&body=aws&tagged=python&site=stackoverflow&key=e7vMoB4rv7qyegz6yaH3QA(('
+
+  fromdate.secs = as.integer(fromdate) * 60 * 60 * 24
+  # the last second is left off so that the intervals are completely covered
+  # and do not overlap
+  todate.secs = as.integer(todate) * 60 * 60 * 24 - 1
+  
+  tagged = URLencode(tagged, reserved = TRUE)
+  body = URLencode(body, reserved = TRUE)
+
+  url = 
+    sprintf( 
+      'http://api.stackexchange.com/2.2/search/advanced?fromdate=%d&todate=%d&order=desc&sort=activity&body=%s&tagged=%s&site=stackoverflow&key=%s&pagesize=100',
+       fromdate.secs, todate.secs, body, tagged, app.key
+    )
+  
+  print(url)
+
+  retry(
+    {
+      req = GET(url) 
+
+      if (req$status_code != 200) {
+        sleep(20)
+        stop(sprintf("http status of %d received", req$status_code))
+      }
+    }, 
+    times = num.retries)
+  
+  co.req = content(req)
+  
+  ans = length(co.req$items)
+  attr(ans, "has_more") = co.req$has_more
+  
+  print(ans)  
+  
+  # for crash recovery
+  dbg.last.req <<- req
+
+  if (wait.it) {
+    backoff = req$backoff
+    if (!is.null(backoff)) {
+      print(sprintf("backoff requested: waiting %d seconds", backoff))
+      Sys.sleep(backoff)
+    }
+    else {
+      # do not make more than 30 requests per second
+      Sys.sleep(2)
+    }
+  }
+
+  return(ans)
+}
+
+get.SO.question.counts = function(fromdates, todates,
+                                  body, tagged, app.key, wait.it = TRUE, retries = 3) {
+  
+  if (length(todates) != length(fromdates))
+    stop("fromdates and todates are not of equal length")
+
+  total.count = get.SO.question.count(fromdates[1], tail(todates, 1),
+                                      body, tagged, app.key, 
+                                      wait.it, retries)
+  has_more = attr(total.count, "has_more")
+  
+  if (!has_more && (total.count > 0)) {
+    ans = rep(NA, length(fromdates))
+    if (length(todates) > 1) {
+      for(i in 2:length(fromdates)) {
+        count =  get.SO.question.count(fromdates[i], todates[i],
+                                       body, tagged, app.key,
+                                       wait.it, retries)
+        ans[i] = count
+        has_more = has_more || attr(count, "has_more")
+      }
+    }
+    ans[1] = total.count - sum(ans[-1])
+  } else {
+    ans = rep(0, length(fromdates))
+  }
+  attr(ans, "has_more") = has_more
+  return(ans)
+}
+
+so.scrape = function(searches,
+                     row.cache.filename.fmt,
+                     output.file,
+                     app.key) { 
+
+  start.date = as.Date("2008-01-01")
+  n.periods = 52 * 9
+  http.error.status.base = 400
+  http.error.status.too.many.reqs = 403
+
+  results = list()
+  
+  for (keyword in names(searches)) {
+    results[[keyword]] = list()
+    
+    for (language in searches[[keyword]]) {
+      print(sprintf("keyword: %s language: %s", keyword, language))
+      
+      row.cache.filename = sprintf(row.cache.filename.fmt,  keyword, language)
+      
+      if (file.exists(row.cache.filename)) {
+        df = read.csv(row.cache.filename)
+        counts = df$values
+      } else {
+        counts = c()
+      }
+        
+      nweeks = 52 * 7
+      if (length(counts) < nweeks) {
+        while(nweeks > length(counts)) {
+          first.nweeks.scrape = 
+            get.SO.question.count(start.date, as.Date(7 * nweeks - 1 + start.date), 
+                                  keyword, language, app.key)
+          if (first.nweeks.scrape == 0) {
+            print(sprintf("first %d weeks are 0", nweeks))
+            # TODO: pad nicely, thoughtfully etc.
+            while (length(counts) < nweeks) {
+              counts = c(counts, 0)
+            }
+            break
+          }
+          else {
+            nweeks = nweeks - 52
+          }
+        }
+      }
+      
+      if (length(counts) < n.periods) { # cached data may lean beyond
+        next.cache = 9 * (length(counts) %/% 9) + n.periods %% 9
+        try.more = TRUE
+ 
+        while(length(counts) < n.periods) {
+          i = length(counts)
+          d1 = start.date + i * 7
+          
+          if (try.more && 
+              ((i + 4) <= n.periods)) {
+            d1 = d1 + 7 * (0:3)
+            d2 = d1 + 7
+            print(sprintf("scraping from %s to %s", head(d1), tail(d2)))
+            new.counts =
+              get.SO.question.counts(d1, d2, keyword, language, app.key)
+            if (attr(new.counts, "has_more")) {
+              # do not try to scrape more of these together as 
+              # paging is yet to be implemented
+              #
+              # actually, retry without paging
+              try.more = FALSE
+            } else {
+              counts = c(counts, new.counts)
+            }
+          }
+          else {
+            d2 = d1 + 6
+            print(sprintf("scraping from %s to %s", d1, d2))
+            count = get.SO.question.count(d1, d2, keyword, language, app.key)
+            print(count)
+            if (attr(count, "has_more")) {
+              stop("need a greater resolution/paging is not implemented")
+            }
+            counts[i + 1] = count
+          }
+
+          if (length(counts) >= next.cache) {
+            write.csv(data.frame(values = counts), file = row.cache.filename)
+            next.cache = next.cache + 9
+          }
+        }
+      }
+      
+      results[[keyword]][[language]] = counts
+    }
+    # allow to have a peek on the intermediate results
+    save(file = output.file, results)
+  }
+}
+
+test = function() {
+  ns = 
+    get.SO.question.counts(
+      c(as.Date("2015-01-01"), as.Date("2015-01-07")),
+      c(as.Date("2015-01-07"), as.Date("2015-01-14")),
+      body = "Analysis",
+      tagged = "Python",
+      app.key = "e7vMoB4rv7qyegz6yaH3QA(("
+    )
+  
+  n1 =
+    get.SO.question.count(
+      as.Date("2015-01-01"), 
+      as.Date("2015-01-07"), 
+      body = "Analysis",
+      tagged = "Python",
+      app.key = "e7vMoB4rv7qyegz6yaH3QA(("
+    )
+  
+  n2 = 
+    get.SO.question.count(
+      as.Date("2015-01-07"), 
+      as.Date("2015-01-14"), 
+      body = "Analysis",
+      tagged = "Python",
+      app.key = "e7vMoB4rv7qyegz6yaH3QA(("
+    )
+  
+  if ((n1 != ns[1]) || (n2 != ns[2])) {
+    stop("values do not add up")
+  }
+}
---
+++ so-scraper.R
@@ -0,0 +1,39 @@
+# setwd("/media/janca/Code/Prog/Github Analysis/analytics-and-hadoop-trends/github-trends/")
+source("so-client.R")
+
+row.cache.filename.fmt = "intermediate/SO %s %s created weekly.csv"
+
+output.file = "intermediate/SO_results.RData"
+
+searches =
+  list(
+    "Machine Learning" = c("Python", "Java", "R", "C#"), # added C#! 
+    Analysis = c("R", "Python", "Java", "R", "C#"),  # added C#!
+    Spark = c("Python", "Java", "Scala", "R"), # no c#
+    "Deep Learning" = c("C++", "Java", "MatLab", "Python", "R", "C#"),   # added C#!
+    "Big Data" = c("Python", "Java", "R", "Scala", "C#"),
+    "Kaggle" = c("Python", "R", "Matlab", "Java"), # no C#
+    "Data Science" = c("Python", "R", "Jupyter Notebook", "C#"), # added C#!
+       # pending "Java", "C++", "TeX", "go", 
+    # "Data" = c("Python", "R", "Jupyter Notebook", "Java", "C++", "TeX", "go", "C#"), # added C#!
+    # "Science" = c("Python", "R", "Jupyter Notebook", "Java", "C++", "TeX", "go", "C#"), # added C#!
+    "AWS" = c("JavaScript", "Python", "Ruby", "Java", "PHP", "go", "C#") # added C#!
+    # # "Coursera" = c(
+    # #   "HTML",
+    # #   "R",
+    # #   "CSS",
+    # #   "JavaScript",
+    # #   "Matlab",
+    # #   "Java",
+    # #   "Python",
+    # #   "Scala",
+    # #   "Jupyter Notebook",
+    # #   "Ruby"
+    # # )
+  )
+
+
+so.scrape(searches = searches,
+          row.cache.filename.fmt = row.cache.filename.fmt,
+          output.file = output.file,
+          app.key = "e7vMoB4rv7qyegz6yaH3QA((")
